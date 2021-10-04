5fd7f1a2e968146ac057b680cb60b4ba9dc9fb26
Yuzunoha <studysteadystandby@gmail.com>
2021-10-05 00:33:41 +0900

    timestamps要らん

--- html/laravelapp/database/migrations/2021_10_04_153043_create_threads_table.php
+++ html/laravelapp/database/migrations/2021_10_04_153043_create_threads_table.php
@@ -16,7 +16,6 @@ class CreateThreadsTable extends Migration
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->timestamps();
         });
     }
 
