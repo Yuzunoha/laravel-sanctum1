cda932950c69cd9e9fe6d4431312a13e6cc5156a
Yuzunoha <studysteadystandby@gmail.com>
2021-10-04 16:18:40 +0900

    ddで確認createToken()

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    $user = Auth::loginUsingId(1);
+    $token = $user->createToken('test');
+    dd($token);
 });
