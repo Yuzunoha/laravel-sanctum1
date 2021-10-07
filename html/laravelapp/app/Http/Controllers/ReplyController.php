d7615dcb303023d06e19802584a02b604fdb015c
Yuzunoha <studysteadystandby@gmail.com>
2021-10-08 00:30:57 +0900

    入った

--- html/laravelapp/app/Http/Controllers/ReplyController.php
+++ html/laravelapp/app/Http/Controllers/ReplyController.php
@@ -8,24 +8,16 @@ use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
-    protected $replyServiceInterface;
+    protected $replyService;
 
-    public function __construct(ReplyServiceInterface $replyServiceInterface)
-    {
-        $this->replyServiceInterface = $replyServiceInterface;
+    public function __construct(
+        ReplyServiceInterface $replyService
+    ) {
+        $this->replyService = $replyService;
     }
 
     public function test(Request $request)
     {
-        $this->replyServiceInterface->create(2, 2, 'テキストです。', 'IPアドレスです。');
-        return;
-        $m = new Reply;
-        $m->thread_id = 1;
-        $m->user_id = 1;
-        $m->text = 'テキストです。';
-        $m->ip_address = 'IPアドレスです。';
-
-        $m->save();
-        return $m;
+        return $this->replyService->create(2, 2, 'テキストです。', 'IPアドレスです。');
     }
 }
