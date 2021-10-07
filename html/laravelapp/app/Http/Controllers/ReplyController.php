aea1ff4f765d8cbe3cefcd7f0f856cd6a7043661
Yuzunoha <studysteadystandby@gmail.com>
2021-10-07 22:27:47 +0900

    タイムスタンプも完璧！

--- html/laravelapp/app/Http/Controllers/ReplyController.php
+++ html/laravelapp/app/Http/Controllers/ReplyController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reply;
 use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
     public function test(Request $request)
     {
-        return '呼ばれた';
+        $m = new Reply;
+        $m->thread_id = 1;
+        $m->user_id = 1;
+        $m->text = 'テキストです。';
+        $m->ip_address = 'IPアドレスです。';
+
+        $m->save();
+        return $m;
     }
 }
