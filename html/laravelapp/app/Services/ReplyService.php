2ed9886b1746eb4953ab2e2eee196630db7e10b7
Yuzunoha <studysteadystandby@gmail.com>
2021-10-08 16:51:47 +0900

    ありがたいコメント

--- html/laravelapp/app/Services/ReplyService.php
+++ html/laravelapp/app/Services/ReplyService.php
@@ -16,6 +16,8 @@ class ReplyService implements ReplyServiceInterface
 
     public function create($thread_id, $user_id, $text, $ip_address)
     {
+        /* thread_id が存在するか確認する */
+        /* user_id が存在するか確認する */
         return $this->replyRepository->insert($thread_id, $user_id, $text, $ip_address);
     }
 
