c28b42296dd41aaffbe26b1eac161239e7164c6e
Yuzunoha <studysteadystandby@gmail.com>
2021-10-08 16:47:07 +0900

    リポジトリのユニットテスト

--- html/laravelapp/tests/Unit/ReplyRepositoryTest.php
+++ html/laravelapp/tests/Unit/ReplyRepositoryTest.php
@@ -26,4 +26,23 @@ class ReplyRepositoryTest extends TestCase
         $replyModel = $this->rep->insert($thread_id, $user_id, $text, $ip_address);
         $this->assertDatabaseHas('replies', $replyModel->toArray());
     }
+
+    public function test_selectAll()
+    {
+        $this->rep->insert(1, 1, 'あ', '0.0.0.0');
+        $this->rep->insert(1, 2, 'い', '0.0.0.0');
+
+        $ret = $this->rep->selectAll();
+        $this->assertSame(2, count($ret->toArray()));
+
+        $this->assertEquals(1, $ret[0]->id);
+        $this->assertEquals(1, $ret[0]->thread_id);
+        $this->assertEquals(1, $ret[0]->user_id);
+        $this->assertEquals('あ', $ret[0]->text);
+
+        $this->assertEquals(2, $ret[1]->id);
+        $this->assertEquals(1, $ret[1]->thread_id);
+        $this->assertEquals(2, $ret[1]->user_id);
+        $this->assertEquals('い', $ret[1]->text);
+    }
 }
