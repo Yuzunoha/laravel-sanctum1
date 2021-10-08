736802baf69193f64aff9823f1794f91a06bba4c
Yuzunoha <studysteadystandby@gmail.com>
2021-10-08 16:23:32 +0900

    日本時間の見やすい時刻になった

--- html/laravelapp/app/Models/Reply.php
+++ html/laravelapp/app/Models/Reply.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
     use HasFactory;
+
+    /**
+     * 配列/JSONシリアル化の日付を準備
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
